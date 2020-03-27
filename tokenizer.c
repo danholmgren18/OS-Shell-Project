698fb32308f7567cf128aa0a79a0d1085ade9516
Dan Holmgren <danholmgren18@gmail.com>
2020-03-27 12:30:04 -0400

    Stored tokens in an array, and removed \n from tokens

--- tokenizer.c
+++ tokenizer.c
@@ -12,17 +12,25 @@ char* token;
 
 
 void tokenize(char *str){
+    char *commands[15];
+    int place = 1;
     token = strtok(str, " ");
+    commands[0] = token;
     while(token != NULL){
-        printf("%s\n", token);
+        printf("%s", token);
         token = strtok(NULL, " ");
+        commands[place] = token;
+        place++;
     }
 }
 
 int main(void){
-    char testing[100];
-    strncpy(testing, "This is a test", 100);
-    tokenize(testing);
+    char line[1024];
+    printf("Enter a command: ");
+    fgets(line, sizeof line, stdin);
+    line[strcspn(line, "\n")] = 0;
+
+    tokenize(line);
 }
 
 
