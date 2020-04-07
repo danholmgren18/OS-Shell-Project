e8b423b7411c3c0575e1ac1fa15c3aa1c54391a2
Zachary Semanco <zs3196@cs.ship.edu>
2020-04-06 21:06:29 -0400

    Started fixing stuff

--- sush.c
+++ sush.c
@@ -7,6 +7,14 @@
 
 #define COMMAND_SIZE 60
 
+int command_list_size = 6;
+char *command_list[] = {"cd", "setenv", "unsetenv", "pwd", "exit", "accnt"};
+
+
+void checkInternalCommand(char *cmd);
+void execInternal(int command_num);
+
+
 int main (int argc, char **argv[])
 {
   const char *prompt = "Welcome to the Shippensburg University Shell (SUSH)! Please enter a command\n";
@@ -23,13 +31,84 @@ int main (int argc, char **argv[])
   
   printf("\n\n");
   
-  Token tokens;
+  tokenizer_t tkn;
 
-  tokens = tokenize(command); 
+  tkn = tokenize(command); 
 
-  printf("\nIn Main, token.command_name: %s\n", tokens.command_name);
-  printf("In Main, token.argument: %s\n", tokens.argument);
-  printf("In Main, token.in_quotes: %s\n", tokens.in_quotes);
-  printf("In Main, token.file_name: %s\n", tokens.file_name);
-  printf("In Main, token.redirect: "); printf(tokens.redirect ? "true\n" : "false\n");
+  command_t cmd;
+
+  cmd.cmd = tkn.tokens[0];
+
+  checkInternalCommand(cmd.cmd);
+
+  cmd.argc = tkn.num_tokens-1;
+
+  for(int i = 1; i < tkn.num_tokens; i++)
+  {
+     cmd.args[i-1] = tkn.tokens[i];
+  }
+
+  // parseArgs(cmd.arg);
+
+  // printf("\nIn Main, token.command_name: %s\n", tokens.command_name);
+  // printf("In Main, token.argument: %s\n", tokens.argument);
+  // printf("In Main, token.in_quotes: %s\n", tokens.in_quotes);
+  // printf("In Main, token.file_name: %s\n", tokens.file_name);
+  // printf("In Main, token.redirect: "); printf(tokens.redirect ? "true\n" : "false\n");
 }
+
+
+void checkInternalCommand(char *cmd)
+{
+    for(int i = 0; i < command_list_size - 1; i++)
+    {
+        if(strcmp(command_list[i], cmd) == 0)
+        {
+           execInternal(i);
+        }
+    }
+}
+
+void execInternal(int command_num)
+{
+  
+    printf("command_num = %d\n", command_num);
+    switch(command_num) 
+    {
+        case 0:
+            printf("User entered %s\n", command_list[command_num]);
+            break;
+        case 1:
+            printf("User entered %s\n", command_list[command_num]);
+            break;
+        case 2:
+            printf("User entered %s\n", command_list[command_num]);
+            break;
+        case 3:
+            printf("User entered %s\n", command_list[command_num]);
+            break;
+        case 4:
+            printf("User entered %s\n", command_list[command_num]);
+            break;
+        case 5:
+            printf("User entered %s\n", command_list[command_num]);
+            break;
+        default: 
+            printf("Not an internal command\n");
+    }
+}
+
+// void parseArgs (int argc, char *args[])
+// {
+//     int state = 0;
+
+//     for(int i = 0; i < argc; i++)
+//     {
+//         switch(state)
+//         {
+//             case 0:
+
+//         }
+//     }
+
+// }
--- tokenizer.h
+++ tokenizer.h
@@ -7,20 +7,30 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct _Token 
-{
-    char command_name[64];
-    char argument[64];
-    char in_quotes[64];
-    char file_name[64];
-    int redirect; 
-} Token;
+// typedef struct _Token 
+// {
+//     char command_name[64];
+//     char argument[64];
+//     char in_quotes[64];
+//     char file_name[64];
+//     int redirect; 
+// } Token;
 
-typedef struct _Token2 
+typedef struct _Token 
 {
     int num_tokens;
     char *tokens[20];
 } tokenizer_t;
 
-Token tokenize(char *str); //Function holding tokenizing functionality 
+typedef struct _Command
+{
+    char *cmd;
+    char *in_name;
+    char *out_name;
+    int append;
+    int argc;
+    char *args[];
+} command_t;
+
+tokenizer_t tokenize(char *str); //Function holding tokenizing functionality 
 
